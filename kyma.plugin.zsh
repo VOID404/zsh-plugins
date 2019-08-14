82fca0c1c9aeb6ac809090ebb21221126764b47b
i354967 <wojciech.nawa@sap.com>
2019-08-14 08:28:29 +0200

    Improve kyma.plugin

--- kyma.plugin.zsh
+++ kyma.plugin.zsh
@@ -6,4 +6,5 @@ function kyma-init {
     kubectl get virtualservice core-console -n kyma-system -o jsonpath='{ .spec.hosts[0] }'
     echo ""
     kubectl get secret admin-user -n kyma-system -o jsonpath="{.data.password}" | base64 --decode
+    echo ""
 }
