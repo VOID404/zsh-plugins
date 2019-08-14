113c38a475523c527f94e26f7cae6b84f4a96a64
i354967 <wojciech.nawa@sap.com>
2019-08-14 09:07:59 +0200

    Improve kyma.plugin

--- kyma.plugin.zsh
+++ kyma.plugin.zsh
@@ -3,8 +3,10 @@ function kyma-init {
 	&& kubectl get configmap net-global-overrides -n kyma-installer -o jsonpath='{.data.global\.ingress\.tlsCrt}' | base64 --decode > $tmpfile \
 	&& sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $tmpfile \
 	&& rm $tmpfile
+
     kubectl get virtualservice core-console -n kyma-system -o jsonpath='{ .spec.hosts[0] }'
     echo ""
     kubectl get secret admin-user -n kyma-system -o jsonpath="{.data.password}" | base64 --decode
     echo ""
+    kyma-tiller-tls&
 }
